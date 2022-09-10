7d7a05c6c15ebd90b4e795264d708823130b7950
NadjaRunge <nadja.runge@study.hs-duesseldorf.de>
2022-09-10 09:44:50 +0200

    :sparkles: add picture-folders

---
+++ src/components/Folder.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="place">
+            {{this.folder.place}}
+        </div>
+        <div class="event">
+            {{this.folder.event}}
+        </div> 
+        <div class="date">
+            {{this.folder.date}}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: [
+            "folder"
+        ]
+    }
+</script>
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-content: center;
+        justify-content: center;
+        justify-content: space-between;
+        background-color: white;
+        /* box-shadow: 0px 0px 5px #5c30303e; */
+        padding: 15px;
+        margin: auto;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        border-radius: 11px;
+        max-width: 1500px;
+        width: auto;
+    }
+    .container:hover {
+    box-shadow: 0px 0px 10px #0000003e;
+    transition: all 0.5s;
+}
+</style>  
--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,15 +1,50 @@
 <template>
     <DefaultView titel="Fotos">
-
+        <Folder v-for="_folder in this.folders" :folder="_folder"></Folder>
     </DefaultView>
 </template>
 <script>
     import DefaultView from '../components/DefaultView.vue'
+    import Folder from '../components/Folder.vue'
 
     export default {
         components: {
-            DefaultView
+            DefaultView, Folder
+        },
+        data () {
+            return {
+                folders: [
+                    {
+                        place: "Fischbach", 
+                        event: "Zeltlager", 
+                        date: "1996"
+                    },
+                    {
+                        place: "Oberthal", 
+                        event: "Zeltlager", 
+                        date: "1997"
+                    },
+                    {
+                        place: "Diemelsee", 
+                        event: "Zeltlager", 
+                        date: "2010"
+                    },
+                    {
+                        place: "Büllingen", 
+                        event: "Zeltlager", 
+                        date: "2022"
+                    },
+                    {
+                        place: "Töpper See", 
+                        event: "Kartoffelfeuer", 
+                        date: "2018"
+                    }
+                ]
+            }
         }
     }
     
 </script>
+<style scoped>
+   
+</style>
