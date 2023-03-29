ade311e8d0ad0fbfd4172e8809e6fae5b49c10a4
Florian <florian.seefing@icloud.com>
2023-03-29 17:58:59 +0200

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 const app = createApp(App)
 
