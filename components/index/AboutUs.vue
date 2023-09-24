a4f3882d0062d22c51653ac5eccefea49906c5ef
NadjaRunge <nadja.runge@study.hs-duesseldorf.de>
2023-09-24 14:16:01 +0200

    :sparkles: About us main layout done

--- components/index/AboutUs.vue
+++ components/index/AboutUs.vue
@@ -1,48 +1,18 @@
 <template>
     <div class="b-container">
-
         <div class="text-center">
             <h2>Das sind wir</h2>
             <h3>Echte Frönde stonnt tesaame...</h3>
         </div>
-        <div class="txt text-right">
-            ...unter diesem Mo4o steht die Katholische Pfarrjugend Hüls seit 1978. Spielerisch lernen
-            unsere jungen Mitglieder durch verschiedene AkFvitäten die Bedeutung von FreundschaI,
-            Zusammenhalt und Verantwortung. Neben unseren wöchentlichen Gruppenstunden
-            organisiert die Leiterrunde über das Jahr verteilt verschiedene AkFvitäten.
-        </div>
-        <img class="pic-left" src="~/assets/buellingen_all.jpg" />
-        <div class="txt text-left">
-            Jeden November können sich Kinder ab der vierten Klassen in neu entstehende Mädchenbzw. Jungengruppen einteilen lassen oder jederzeit in eine gleichaltrige Gruppe eintreten.
-            Die Gruppen treffen sich wöchentlich für 90 Minuten mit ihren Leiter:innen im HeinrichJoeppen-Haus, um zu spielen, kochen, backen, basteln, toben und viel zu lachen. Dies stärkt
-            ganz besonders den Zusammenhalt, sodass aus der anfangs bunten Truppe meist ein enger
-            Freundeskreis wird.
-        </div>
-        <img class="pic-right" src="~/assets/buellingen_all.jpg" />
-        <div class="txt text-right">
-            Über das Jahr verteilt organisiert die Leiterrunde verschiedene Ausflüge ins Spaßbad oder
-            den Freizeitpark sowie das Kartoffelfeuer mit einer Rallye oder Fahrradtour und
-            Nachtwanderung mit anschließendem Lagerfeuer. Highlight des Jahres ist unser Zeltlager in
-            der vierten und fünIen Woche der Sommerferien. Die Favoriten im Programm sind sicher
-            der Singsang am Lagerfeuer, die verschiedenen Stafe4en und das Brennball-Turnier, aber
-            auch der gemeinsame Küchen-, Toile4en- oder Ordnungsdienst macht eine Menge Spaß.
-        </div>
-        <img class="pic-left" src="~/assets/buellingen_all.jpg" />
-        <div class="txt text-right">
-            <div class="txt text-right">
-            Über das Jahr verteilt organisiert die Leiterrunde verschiedene Ausflüge ins Spaßbad oder
-            den Freizeitpark sowie das Kartoffelfeuer mit einer Rallye oder Fahrradtour und
-            Nachtwanderung mit anschließendem Lagerfeuer. Highlight des Jahres ist unser Zeltlager in
-            der vierten und fünIen Woche der Sommerferien. Die Favoriten im Programm sind sicher
-            der Singsang am Lagerfeuer, die verschiedenen Stafe4en und das Brennball-Turnier, aber
-            auch der gemeinsame Küchen-, Toile4en- oder Ordnungsdienst macht eine Menge Spaß.
-        </div>
-        <img class="pic-left" src="~/assets/buellingen_all.jpg" />
-        </div>
-        <img class="pic-left" src="~/assets/buellingen_all.jpg" />
-        <div class="txt text-right">
-            Hast du Lust bekommen, ein Teil der KPJH zu werden? Dann besuch uns doch mal oder
-            fordere mehr Infos über unser Kontakiormular an.
+        <div class="grid-container">
+            <div class="grid header">...unter diesem Motto steht die Katholische Pfarrjugend Hüls seit 1978. Spielerisch lernen unsere jungen Mitglieder durch verschiedene Aktivitäten die Bedeutung von Freundschaft, Zusammenhalt und Verantwortung. Neben unseren wöchentlichen Gruppenstunden organisiert die Leiterrunde über das Jahr verteilt verschiedene Aktivitäten.</div>
+            <div class="grid text-1">Jeden November können sich Kinder ab der vierten Klassen in neu entstehende Mädchen- bzw. Jungengruppen einteilen lassen oder jederzeit in eine gleichaltrige Gruppe eintreten. Die Gruppen treffen sich wöchentlich für 90 Minuten mit ihren Leiter:innen im Heinrich-Joeppen-Haus, um zu spielen, kochen, backen, basteln, toben und viel zu lachen. Dies stärkt ganz besonders den Zusammenhalt, sodass aus der anfangs bunten Truppe meist ein enger Freundeskreis wird.</div>
+            <div class="grid pic-1"><img class="grid-pic" src="~/assets/buellingen.jpg" /></div>
+            <div class="grid text-2">Über das Jahr verteilt organisiert die Leiterrunde verschiedene Ausflüge ins Spaßbad oder den Freizeitpark sowie das Kartoffelfeuer mit einer Rallye oder Fahrradtour und Nachtwanderung mit anschließendem Lagerfeuer. Highlight des Jahres ist unser Zeltlager in der vierten und fünften Woche der Sommerferien. Die Favoriten im Programm sind sicher der Singsang am Lagerfeuer, die verschiedenen Stafetten und das Brennball-Turnier, aber auch der gemeinsame Küchen-, Toiletten- oder Ordnungsdienst macht eine Menge Spaß.</div>
+            <div class="grid pic-2"><img class="grid-pic" src="~/assets/buellingen.jpg" /></div>
+            <div class="grid pic-3"><img class="grid-pic" src="~/assets/buellingen.jpg" /></div>
+            <div class="grid text-3">Seit 20XX sind wir als Ortsgruppe „KjG Pfarrjugend Hüls“ der Katholischen jungen Gemeinde (KjG) im Bistum Aachen aktiv. Dies ermöglicht uns spezielle Angebote für unsere Kindergruppen und Leiter:innen wie Gruppenstundenangebote, Erlebniswochenenden oder Fortbildungen anzubieten.</div>
+            <div class="grid footer">Hast du Lust bekommen, ein Teil der KPJH zu werden? Dann besuch uns doch mal oder fordere mehr Infos über unser Kontaktformular an.</div>
         </div>
     </div>
 </template>
@@ -51,8 +21,9 @@
 </script>
 <style scoped lang="scss">
 
-.b-container {
-    display: absolute;
+h2 {
+    padding: 4rem;
+    margin-top: 6rem;
 }
 
 h3 {
@@ -60,48 +31,73 @@ h3 {
     font-size: 50px;
 }
 
-h2 {
-    padding: 4rem;
-}
-
 .text-center {
     align-self: center;
 }
-.txt {
+
+.grid-container {
+    display: grid;
+    justify-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas: 
+    'header header header'
+    'text text pic'
+    'pic2 text2 text2'
+    'text3 text3 pic3'
+    'footer footer footer';
     margin: 20px;
-    line-height: 2.8;
-    text-indent: 10px;
-    margin-top: 100px;
-    display: flex;
-    position: inherit;
+    line-height: 2;
+    margin: 10rem;
+    margin-bottom: 80px;
+    justify-content: space-between;
+    align-items: center;
 }
 
-.text-right {
-    text-align: right;
+.grid {
+    margin-bottom: 2rem;
 }
 
-.text-left {
-    align-self: left;
+.header {
+    grid-area: header;
 }
 
-.pic-right {
-    align-self: right;
-    width: 350px;
-    height: 100%;
-    z-index: 0;
+.text-1{
+    grid-area: text;
+}
+.pic-1{
+    grid-area: pic;
     transform: rotate(7deg);
-    opacity: 0.6;
 
 }
-
-.pic-left {
-    align-self: left;
-    width: 350px;
-    height: 100%;
-    z-index: 0;
-    transform: rotate(-7deg);
-    opacity: 0.6;
-    position: relative;
+.text-2{
+    grid-area: text2;
 }
+
+.pic-2 {
+    grid-area: pic2;
+    transform: rotate(-7deg) translateX(-30px);
+}
+
+.text-3{
+    grid-area: text3;
+}
+
+.pic-3 {
+    grid-area: pic3;
+    transform: rotate(7deg) translateX(10px);
+}
+
+.footer {
+    grid-area: footer;
+}
+
+.grid-pic {
+    align-self: center;
+    width: 100%;
+    height: auto;
+}
+
+
 
 </style>
--- composables/authComps.js
+++ composables/authComps.js
@@ -1,7 +1,10 @@
 export const useLogout = () => {
-	useCookie('token').value = null
+	// useCookie('token').value = null
+	const authCookie = useCookie('token')
+	authCookie.value = null
 	useState('loggedIn').value = false
 	useState('userType').value = null
+	console.log('logged out')
 }
 
 export const useLogin = () => {
